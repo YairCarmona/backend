7dc8b5b8f073c1b449d2efc08b432dca01034da0
Developer Yair <yairc407@gmail.com>
2025-03-25 19:08:36 -0600

    Feat -Creacion del proyecto base

---
+++ convenios-bolsa-trabajo/src/main/java/com/planeacion/vinculacion/convenios/bolsa/trabajo/ConveniosBolsaTrabajoApplication.java
@@ -0,0 +1,13 @@
+package com.planeacion.vinculacion.convenios.bolsa.trabajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConveniosBolsaTrabajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConveniosBolsaTrabajoApplication.class, args);
+	}
+
+}
---
+++ convenios-bolsa-trabajo/src/test/java/com/planeacion/vinculacion/convenios/bolsa/trabajo/ConveniosBolsaTrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planeacion.vinculacion.convenios.bolsa.trabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveniosBolsaTrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
